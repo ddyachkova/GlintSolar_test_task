c2d3cd68e2ada892fd48a07e56eda40958b3556c
ddyachkova <darya.dyachkova@minerva.kgi.edu>
2021-01-05 21:54:15 +0200

    add files via upload

---
+++ app.py
@@ -0,0 +1,36 @@
+import flask
+import requests 
+from flask import Flask, render_template, request
+
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return "Welcome to the starting page"
+
+@app.route('/foo', methods=['POST']) 
+def foo():
+    data = request.json
+    print (data)
+    return jsonify(data)
+
+
+@app.route('/input/', methods=['post', 'get'])
+def login():
+    message = ''
+    if request.method == 'POST':
+        lattitude = request.form.get('lattitude')  # access the data inside 
+        longitude = request.form.get('longitude')
+
+        url = 'https://europe-west1-freesolarcalc.cloudfunctions.net/wave_max/'
+        params = {"lat":float(lattitude), "lon":float(longitude)}
+        res = requests.get(url, params)
+        message = res.text
+    return render_template('input.html', message=message)
+#...
+
+
+app.run()
